96895d4ced2e6fa617af30d954dd9df50a94ff1a
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-06 15:18:30 -0600

    seed

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,8 +1,10 @@
 import { BasePayload } from 'payload'
 import { seedCustomers } from './customers'
+import { ensureRootAdmin } from '@/scripts/create-initial-admin'
 
 export const seed = async ({ payload }: { payload: BasePayload }) => {
   console.log('🌱 Iniciando seed...')
+  await ensureRootAdmin(payload)
   await seedCustomers(payload)
   console.log('✅ Seed completado')
 
