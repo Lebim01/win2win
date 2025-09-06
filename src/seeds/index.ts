eb18b3ab30eeb5ace8ae47676aaec02ca22021a2
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-06 14:54:58 -0600

    seed

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,12 +1,7 @@
 import { BasePayload } from 'payload'
 import { seedCustomers } from './customers'
-import payloadConfig from '@payload-config'
 
 export const seed = async ({ payload }: { payload: BasePayload }) => {
-  await payload.init({
-    config: payloadConfig,
-  })
-
   console.log('🌱 Iniciando seed...')
   await seedCustomers(payload)
   console.log('✅ Seed completado')
