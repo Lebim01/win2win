cd860e8d1baf8ad00814a3a134c1e3d7ee05c4de
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-30 14:12:08 -0700

    cupones

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -24,12 +24,32 @@ const Dashboard = async ({ payload, user, searchParams }: AdminViewServerProps) 
     },
   })
 
+  const bonus_total = await payload.db.drizzle
+    .execute(
+      `
+    SELECT SUM(amount) as _total FROM public."referral_payouts"
+  `,
+    )
+    .then((r: any) => r.rows[0])
+
+  const used_coupons = await payload.count({
+    collection: 'coupons',
+    where: {
+      redeemed: {
+        equals: true,
+      },
+    },
+  })
+
   return (
     <div className="view-container">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <Indicator title="Usuarios totales" value={customers.totalDocs} />
         <Indicator title="Usuarios activos" value={customers_active.totalDocs} />
         <Indicator title="Usuarios en red" value={customers_network.totalDocs} />
+
+        <Indicator title="Comisiones pagadas" value={bonus_total._total + ' USD'} />
+        <Indicator title="Cupones usados" value={used_coupons.totalDocs} />
       </div>
     </div>
   )
